9269c68533cacde0fcdcd6806269c0ef87383b15
sai131 <92074749+sai131@users.noreply.github.com>
2021-10-23 10:12:03 +0530

    Add files via upload

---
+++ sai program.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main( )
+{
+	// variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	//initialization
+	age = 20;
+	cgpa =10.0;
+	initial = 'D';
+	
+	//output
+	printf("Age of the student is %d\n",age);
+	printf("Cgpa of the student%f\n",cgpa);
+	printf("Student surname starts with %c",initial);
+	}
