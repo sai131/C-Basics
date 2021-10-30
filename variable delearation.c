676783b0ee802fd322344b602d4bcc818ac0eebe
sai131 <92074749+sai131@users.noreply.github.com>
2021-10-30 10:28:27 +0530

    Add files via upload

---
+++ variable delearation.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main( )
+{
+	//variable declaration
+	int maths,english,physics,telugu,sum;
+	//instalisation
+	maths=50;
+	english=53;
+	physics=45;
+	telugu=60;
+	//calculation
+	sum=maths+english+physics+telugu;
+	//output
+	printf("maths%d\nenglish%d\nphysics%\ntelugu%d\ntotal%d\n",maths,english,physics,telugu,sum);
+	
+}
