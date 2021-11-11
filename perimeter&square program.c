37144688fac86a44aaa0ec7c1c501e057d10ec4b
sai131 <92074749+sai131@users.noreply.github.com>
2021-11-11 10:41:56 +0530

    Add files via upload

---
+++ perimeter&square program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int side,area,perimeter;
+	printf("Enter side length");
+	scanf("%d",&side);
+	area=side*side;
+	perimeter=4*side;
+	printf("area of square is%d\n",area);
+	printf("primetet of square is%d",perimeter);
+	
+
+}
