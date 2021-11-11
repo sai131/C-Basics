2944a62121d899025bf00bfba27669bde00cb780
sai131 <92074749+sai131@users.noreply.github.com>
2021-11-11 10:08:32 +0530

    Add files via upload

---
+++ square programs.c
@@ -0,0 +1,9 @@
+// program to print square of the number
+#include<stdio.h>
+void main()
+{
+	int a,b,c,d;
+	printf("Enter four numbers");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	printf("%d",a*b*c*d);
+}
